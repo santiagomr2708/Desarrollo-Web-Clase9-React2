f60abea060670db7c51a7f02667724dab493609c
Hajhosua <josue.gonzalez043@pascualbravo.edu.co>
2023-05-05 20:26:19 -0500

    Cambios
    Hice unos cambios

--- react2/src/componentes/Navbar.js
+++ react2/src/componentes/Navbar.js
@@ -5,31 +5,36 @@ import {Link} from 'react-router-dom'
 export const Navbar=() =>{
     return(
         <div>
-            <nav class="container">
+
+            <nav>
+            <ul class="container">
                
                     <div class="container-logo">
-                     <h2><span>Logo</span> <span>Pague</span></h2>
+
+                        
+                     <h2><span>Fonda  </span> Mirador<span>.</span></h2>
                     </div>
 
 
                     <ul class="links">   
-                    <li>
-                        <Link to="/">Inicio</Link>
-                    </li>
-                    <li>
-                        <Link to="/empresa">Carne</Link>
-                    </li>
-                    <li>
-                        <Link to="/contacto">Hamburguesa</Link>
-                    </li>
-                    <li>
-                        <Link to="/conocenos">Bebidas</Link>
-                    </li>
+            
+                        <Link class="link" to="/">Inicio</Link>
+                    
+                    
+                        <Link class="link" to="/empresa">Carne</Link>
+                    
+                
+                        <Link class="link" to="/contacto">Hamburguesa</Link>
+                    
+                    
+                        <Link  class="link"  to="/conocenos">Bebidas</Link>
+                    
                     </ul>
 
                     <div class="toggle">
                         |||
                     </div>
+            </ul>
             </nav>
         </div>
     )
